e46bfe7f92d8a3b42805f740ff12e3457782ee7d
Jose Abell <jaabell@ucdavis.edu>
2015-11-24 21:35:42 -0800

    fixed again

--- avtvisitESSIFileFormat.C
+++ avtvisitESSIFileFormat.C
@@ -954,14 +954,14 @@ avtvisitESSIFileFormat::GetVar(int timestate, int domain, const char *varname)
         // GO_HERE << "ncells[domain] = " << ncells[domain] << endl;
         rv = vtkIntArray::New();
         rv->SetNumberOfTuples(ncells[domain]);
-        // int cell_number;
+        int cell_number = 0;
         for (int ii = 0 ; ii < ncells[domain] ; ii++)
         {
             // cell_number = m_tags2cellnumbers[domain][ii];
             // GO_HERE << "cell_number = " << cell_number << ",  tag = " << tag << ", partition = "<< partition[tag] << endl;
             // if (cell_number >= 0)
             // {
-            rv->SetTuple1(cell_number, domain + 1);
+            rv->SetTuple1(cell_number++, domain + 1);
             // }
         }
         // delete [] partition;
